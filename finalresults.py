d7740584730d608d9cee31206ef7ff1599e3003e
al-arsenault <a.arsenault81@gmail.com>
2016-04-09 14:34:20 -0400

    Update finalresults.py

--- finalresults.py
+++ finalresults.py
@@ -3,24 +3,23 @@ from psycopg2 import connect
 import sys
 import getopt
 
-def main(argv)
 
-  conn = psycopg2.connect(database="Tcount", user="postgres", password="pass", host="localhost", port="5432")
-  cur = conn.cursor()
+conn = psycopg2.connect(database="Tcount", user="postgres", password="pass", host="localhost", port="5432")
+cur = conn.cursor()
 
 
-  if len(sys.argv) == 1
+if len(sys.argv) == 1:
      records = cur.fetchall()
      for rec in records:
        print "word = ", rec[0]
-       print "count = ", rec[1], \n
+       print "count = ", rec[1]
      conn.commit()
 
-  else:
+else:
     uword = str(sys.arg1)
     cur.execute("SELECT word, count from Tweetwordcount" WHERE word = uword)
     print("Total number of occurences of %s : %d" %(word, count))
 
     conn.commit()
 #
-  conn.close()
+conn.close()
